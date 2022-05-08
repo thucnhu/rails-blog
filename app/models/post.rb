46cb7b1f21179574996cff3694f6c02f7953676f
thucnhu <buitranthucnhu@gmail.com>
2022-05-08 21:44:46 +0700

    add authentication for user accounts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-   validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
 end
