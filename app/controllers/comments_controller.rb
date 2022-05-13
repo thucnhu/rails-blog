fb115ce110555b5295c77a72a98480837bcd9dac
thucnhu <buitranthucnhu@gmail.com>
2022-05-13 21:46:29 +0700

    comments create and destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,33 @@
 class CommentsController < ApplicationController
    before_action :authenticate_user!
+   before_action :set_post
+
+   def set_post
+      @post = Post.find(params[:post_id])
+   end
 
    def create
+      @comment = @post.comments.create(comment_params)
+      @comment.user = current_user
 
+      if @comment.save
+         flash[:success] = "Comment created!"
+         redirect_to post_path(@post)
+      else
+         flash[:alert] = "Comment not created!"
+         redirect_to post_path(@post)
+      end
    end
 
    def destroy
-
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      flash[:success] = "Comment deleted!"
+      redirect_to post_path(@post)
    end
 
    private
-   def comment_paramUsers
+   def comment_params
       params.require(:comment).permit(:body)
    end
 end
